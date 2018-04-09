1f2dbdce00972f6b6de7cc62a20244955c2647f6
becker181u <becker181u@etu.univ-lorraine.fr>
2018-04-09 14:53:17 +0200

    entete.php

--- en-tete.php
+++ en-tete.php
@@ -1,3 +1,125 @@
 <!DOCTYPE html>
-<html lang="en"
-</html>
+
+<html lang="en">
+
+
+
+  <head>
+
+
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="">
+
+    <meta name="author" content="">
+
+
+
+    <title>Freelancer - Start Bootstrap Theme</title>
+
+
+
+    <!-- Bootstrap core CSS -->
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+    <!-- Custom fonts for this template -->
+
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+
+
+    <!-- Plugin CSS -->
+
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+
+
+    <!-- Custom styles for this template -->
+
+    <link href="css/freelancer.min.css" rel="stylesheet">
+
+
+
+  </head>
+
+
+
+  <body id="page-top">
+
+
+
+    <!-- Navigation -->
+
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+
+      <div class="container">
+
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+
+        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+
+          Menu
+
+          <i class="fa fa-bars"></i>
+
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+
+          <ul class="navbar-nav ml-auto">
+
+            <li class="nav-item mx-0 mx-lg-1">
+
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
+
+            </li>
+
+            <li class="nav-item mx-0 mx-lg-1">
+
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
+
+            </li>
+
+            <li class="nav-item mx-0 mx-lg-1">
+
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
+
+            </li>
+
+          </ul>
+
+        </div>
+
+      </div>
+
+    </nav>
+
+
+
+    <!-- Header -->
+
+    <header class="masthead bg-primary text-white text-center">
+
+      <div class="container">
+
+        <img class="img-fluid mb-5 d-block mx-auto" src="img/profile.png" alt="">
+
+        <h1 class="text-uppercase mb-0">Start Bootstrap</h1>
+
+        <hr class="star-light">
+
+        <h2 class="font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
+
+      </div>
+
+    </header>
