5e99171810346db1c2d0a58fc70680768e9f3cac
Alexandre Sirjean <alexandre.sirjean@gmail.com>
2018-04-09 14:42:45 +0200

    header-modif-
    Encore pour toi beau Yann

--- en-tete.php
+++ en-tete.php
@@ -1,3 +1,17 @@
 <!DOCTYPE html>
-<html lang="en"
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Borne solidaire</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
 </html>
