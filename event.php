1c9a79c48a273b93763a0b4c4461063035ffdd17
becker181u <becker181u@etu.univ-lorraine.fr>
2018-04-12 23:35:13 +0200

    css fini borne

---
+++ event.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Borne solidaire</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row header">
+		<div class="col-md-2">
+			11:06
+		</div>
+		<div class="col-md-8 title">
+			Borne Solidaire
+		</div>
+		<div class="col-md-2 date">
+			Lundi 9 avril 2018
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="page-header">
+						<h1>
+							Centre d'accueil 1<small>à 200m</small>
+						</h1>
+					</div>
+				</div>
+				<div class="col-md-4">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-7 white-pane">
+					<h2>
+						Description
+					</h2>
+					<p>
+						Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+					</p>
+					<p>
+						<a class="btn" href="#">View details »</a>
+					</p>
+				</div>
+				<div class="col-md-5">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10532.84831237213!2d6.187104589271068!3d48.701427275316085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794983f43753b89%3A0xde6d2e9a5224908d!2sAccueil+et+R%C3%A9insertion+Sociale!5e0!3m2!1sfr!2sfr!4v1523462501749" width="600" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-8">
+		</div>
+		<div class="col-md-2">
+		</div>
+	</div>
+</div>
+	
