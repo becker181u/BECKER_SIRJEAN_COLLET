ec01b163ff98264c2c91ea6ae9085aaf850ffaae
becker181u <becker181u@etu.univ-lorraine.fr>
2018-04-12 10:12:03 +0200

    modif dossier

--- en-tete.php
+++ en-tete.php
@@ -44,7 +44,9 @@
     <!-- Custom styles for this template -->
 
     <link href="css/freelancer.min.css" rel="stylesheet">
-	<link href="css/style_admin.css" rel="stylesheet">
+	 
+
+    <link href="css/style_admin.css" rel="stylesheet">
 
 
 
@@ -52,61 +54,10 @@
 
 
 
-  <body id="page-top">
-
-
-
-    <!-- Navigation -->
-
-    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
-
-      <div class="container">
-
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Bornes Solidaires</a>
-
-        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-
-          Menu
-
-          <i class="fa fa-bars"></i>
-
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-
-          <ul class="navbar-nav ml-auto">
-
-            <li class="nav-item mx-0 mx-lg-1">
-
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Inscription</a>
-
-            </li>
-
-            <li class="nav-item mx-0 mx-lg-1">
-
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Connexion</a>
-
-            </li>
-
-            <li class="nav-item mx-0 mx-lg-1">
-
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
-
-            </li>
-
-          </ul>
-
-        </div>
-
-      </div>
-
-    </nav>
-
-
 
     <!-- Header -->
 
-    <header class="masthead bg-primary text-white text-center">
+    <header class="masthead bg-secondary text-white text-center">
 
       <div class="container">
 
---
+++ nbePlaces.php
@@ -0,0 +1,50 @@
+</!DOCTYPE html>
+<html lang="fr">
+	<body>
+	<?php 
+		include("openbdd.php");
+		include("en-tete.php");
+	?>
+	<?php 
+	include("openbdd.php");
+	if (isset($_POST['NbePlace']) && isset($_SESSION['identifiant'])){
+		$N=$_POST['NbePlace'];
+		$requete = 'UPDATE(membres SET nbePlace=$N WHERE identifiant=$_SESSION[identifiant])'; 
+		if ($reponse= $bdd->query($requete)){ ?>
+			<p class="enr">Enregistrement terminée </p><br/>
+			<form  action ="index.php" method="post">
+				<input  type="submit" value="Retour" />
+			</form>
+		<?php }
+		else{ ?>
+			<p class="enr">Problème d'écriture </p><br/>
+			<form  action ="ajout.php" method="post">
+				<input  type="submit" value="Recommencer" />
+			</form>
+		<?php }
+
+	}
+	else{
+		if (isset($_SESSION['identifiant'])) {
+			?>
+			<h1>Ajouter un produit</h1>
+			<form method="post" id="Bouton">
+				<div class="formulaire">
+					<label for="NbePlace">Nombre de places disponibles : </label><br/>
+					<input type="text" name="NbePlace" required/>
+				</div>
+				<br/>
+				<input type="submit" value="Envoyer" />
+			</form>	
+			<?php
+		}
+		else{ ?>
+			<p class="enr">Veuillez vous connecter</p><br/>
+			<form  action ="connexion.php" method="post">
+				<input  type="submit" value="Se connecter" />
+			</form>
+		<?php }
+	} ?>
+	<?php include("pied_de_page.php") ?>
+	</body>
+</html>
